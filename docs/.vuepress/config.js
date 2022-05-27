c1221b631c3c0019bf51fded840f7f46cc109137
zhangguo <cucurbitbaby@163.com>
2022-05-27 23:09:15 +0800

    ✨ feat(前端): 嵌套路由 1. 路由分组嵌套

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,6 +5,22 @@ module.exports = {
       { text: 'Home', link: '/' },
       { text: 'About', link: '/about/' },
       { text: 'External', link: 'https://google.com' },
+      {
+        text: 'Languages',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'Chinese', link: '/language/chinese/' },
+          { text: 'Japanese', link: '/language/japanese/' },
+          { text: '分组1', items: [
+            { text: 'Home', link: '/' },
+            { text: 'About', link: '/about/' },
+          ]},
+          { text: '分组2', items: [
+            { text: 'Home', link: '/' },
+            { text: 'About', link: '/about/' },
+          ]},
+        ]
+      }
     ]
   }
 }
