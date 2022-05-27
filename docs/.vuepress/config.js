2ad2f2092f2a228c57d5131e83eb206d25056622
zhangguo <cucurbitbaby@163.com>
2022-05-27 23:52:18 +0800

    ✨ feat: 数组侧边栏嵌套

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -30,8 +30,17 @@ module.exports = {
     ],
     sidebar: [
       '',
-      'news',
-      'news2'
+      {
+        title: '美丽的 CSS',   // 必要的
+        path: '/css/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          '/css/c-aaa',
+          '/css/c-bbb',
+          '/css/c-ccc',
+        ]
+      },
     ]
   }
 }
