a3f9d05ce23a561d2a3f90734407b82bfc53d0f9
zhangguo <cucurbitbaby@163.com>
2022-05-30 22:24:07 +0800

    ✨ feat:  路由对象的写法 1. 插了一个插件插件广告

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,19 +28,40 @@ module.exports = {
         ]
       }
     ],
-    sidebar: [
-      '',
-      {
-        title: '美丽的 CSS',   // 必要的
-        path: '/css/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        collapsable: false, // 可选的, 默认值是 true,
-        sidebarDepth: 1,    // 可选的, 默认值是 1
-        children: [
-          '/css/c-aaa',
-          '/css/c-bbb',
-          '/css/c-ccc',
-        ]
-      },
-    ]
+    // sidebar: [
+    //   '',
+    //   {
+    //     title: '美丽的 CSS',   // 必要的
+    //     path: '/css/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+    //     collapsable: false, // 可选的, 默认值是 true,
+    //     sidebarDepth: 1,    // 可选的, 默认值是 1
+    //     children: [
+    //       '/css/c-aaa',
+    //       '/css/c-bbb',
+    //       '/css/c-ccc',
+    //     ]
+    //   },
+    // ]
+    sidebar: {
+      // 首页错误示范
+      // '/': [
+      //   ''
+      // ],
+      '/css/': [
+        'c-aaa',
+        'c-bbb',
+        'c-ccc'
+      ],
+      '/javascript/': [
+        'j-aaa',
+        'j-bbb',
+        'j-ccc'
+      ],
+      // 如果上面是/frontend/javascript/,注册/frontend/应该放到下面
+      // 首页正确示范
+      '/': [
+        ''
+      ],
+    }
   }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,10 @@
 # Hello VuePress
-这是我的第一片 VuePress!
+这是我的第一篇 VuePress!
 
 ## 二级标题！
 ## 二级标题！！
-## 二级标题！！！
+## 二级标题 搜索锚点！！！
+## 书签
+[【啰里啰嗦】一步步搭建 VuePress 及优化 ](https://www.bilibili.com/video/BV1vb411m7NY?p=4&spm_id_from=pageDriver)  
+
+[VuePress 中文文档 | VuePress 中文网](https://www.vuepress.cn/guide/)
